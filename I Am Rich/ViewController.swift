a0917b6307dc8da69bc6fe9308b156e1cd672b99
Mahendra Sahu <mahendra.sahu@xcelpros.com>
2020-05-22 14:43:47 +0530

    Initial Commit

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Mahendra on 22/05/20.
+//  Copyright © 2020 Mahendra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
